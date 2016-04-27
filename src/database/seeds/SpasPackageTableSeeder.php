d80b68a9887043035f676cf305bae308a7f40bf5
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-27 18:28:29 +0200

    minor fix

--- src/database/seeds/SpasPackageTableSeeder.php
+++ src/database/seeds/SpasPackageTableSeeder.php
@@ -8,7 +8,7 @@ class SpasPackageTableSeeder extends Seeder
     public function run()
     {
         Package::insert([
-            ['id_012' => '14', 'name_012' => 'Spas Package', 'folder_012' => 'spas', 'sorting_012' => 14, 'active_012' => '0']
+            ['id_012' => '14', 'name_012' => 'Spas Package', 'folder_012' => 'spas', 'sorting_012' => 14, 'active_012' => false]
         ]);
     }
 }
