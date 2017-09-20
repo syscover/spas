bf3dbf9e2f3c937d9912d72d4cab613cda7346ab
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-09-20 19:29:12 +0200

    base_lang2()

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -2,7 +2,7 @@
     <a href="javascript:void(0)"><i class="fa fa-tint"></i>{{ trans('spas::pulsar.package_name') }}</a>
     <ul class="sub-menu">
         @if(is_allowed('spas-spa', 'access'))
-            <li{!! is_current_resource('spas-spa') !!}><a href="{{ route('spa', [base_lang()->id_001]) }}"><i class="fa fa-tint"></i>{{ trans_choice('spas::pulsar.spa', 2) }}</a></li>
+            <li{!! is_current_resource('spas-spa') !!}><a href="{{ route('spa', [base_lang2()->id_001]) }}"><i class="fa fa-tint"></i>{{ trans_choice('spas::pulsar.spa', 2) }}</a></li>
         @endif
     </ul>
 </li>
